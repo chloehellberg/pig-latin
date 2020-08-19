5a57b60af21ffca670874ca1e67ca57d8ad30d16
Jeff Dinsmore <hello@jeffdinsmore.com>
2020-08-19 16:28:14 -0700

    move js code into user interface logic, create UI with input, output and button to display first test function
    Co-authored-by: Chloe Hellberg <chloe.hellberg@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,41 @@
+// Business Logic
 
-const vowels = ["a", "i", "e", "o", "u"];
-const string = "Replace all of the vowels with an X, please!";
-const stringArray = string.split(" ");
-let xArray = [];
-stringArray.forEach(function(word) {
-  console.log(word[0]);
-  if (vowels.includes(word[0].toLowerCase())) {
-    xArray.push(word + "way");
-  } else {
-    xArray.push(word);
-  }
+
+//User Interface Logic
+
+$(document).ready(function() {
+  $("form#form").submit(function(event) {
+    event.preventDefault();
+    const vowels = ["a", "i", "e", "o", "u"];
+    const string = $("input#input1").val();
+    const stringArray = string.split(" ");
+    let xArray = [];
+    stringArray.forEach(function(word) {
+    if (vowels.includes(word[0].toLowerCase())) {
+      xArray.push(word + "way");
+    } else {
+      xArray.push(word);
+    }
 });
 const finalString = xArray.join(" ");
+
+    $("#output").text(finalString);
+    
+  });
+});
+
+
+
+// const vowels = ["a", "i", "e", "o", "u"];
+// const string = $("input#input1").val();
+// const stringArray = string.split(" ");
+// let xArray = [];
+// stringArray.forEach(function(word) {
+//   console.log(word[0]);
+//   if (vowels.includes(word[0].toLowerCase())) {
+//     xArray.push(word + "way");
+//   } else {
+//     xArray.push(word);
+//   }
+// });
+// const finalString = xArray.join(" ");
